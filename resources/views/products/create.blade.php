89f113364e99895a0913179eec3494b100084587
huei20251002 <avachen031@gmail.com>
2026-01-21 09:37:42 +0800

    完成新增商品預覽圖片功能

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -17,9 +17,30 @@
 
     <div>
         <label for="">商品圖片：</label>
-        <input type="file" name="img">
+        <input type="file" name="img" id="imgInput" accept="image/*">
+    </div>
+
+    <div style=" margin-top: 10px">
+        <p>圖片預覽</p>
+        <img id="preview" src="#" alt="預覽圖" style="width: 200px; dispaly: none; border: 1px solid black; padding: 5px;">
     </div>
 
     <button type="submit">儲存商品</button>
 
 </form>
+
+<script>
+document.getElementById('imgInput').onchange = function(evt) {
+
+    // console.log(this.files)
+    const [file] = this.files;
+    // const file = this.files[0];
+
+    if (file) {
+        const preview = document.getElementById('preview');
+        preview.src = URL.createObjectURL(file)
+        preview.style.display = 'block'
+    }
+
+}
+</script>
